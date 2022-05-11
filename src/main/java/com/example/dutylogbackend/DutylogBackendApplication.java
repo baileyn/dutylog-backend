f53043e7246f4677d9a2f551bfbc61f9b63f744c
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:43 -0400

    Initial commit.

---
+++ src/main/java/com/example/dutylogbackend/DutylogBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.dutylogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutylogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutylogBackendApplication.class, args);
+	}
+}
